42fa14978464205cfa118f9c3361c98633c2550f
ardisyam <ardisyam@gmail.com>
2023-08-28 20:42:18 +0930

    exception, logger & utils added

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.mlProject import logger
+
+from src.mlProject.pipeline.
+
+
+
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+logger.info("Welcome to our custom logging")
